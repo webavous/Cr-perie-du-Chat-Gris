d46ff974e862dadfb753ca63c43be54ad8dd0144
webavous <webavous@gmail.com>
2026-08-20 11:25:10 +0200

    Update save.js

--- api/save.js
+++ api/save.js
@@ -1,7 +1,7 @@
 const { getSupabase } = require("./_supabase");
 const { verifyToken, getTokenFromReq } = require("./_auth");
 
-const ALLOWED = ["hours", "closure", "specials", "menu", "gallery"];
+const ALLOWED = ["hours", "closure", "specials", "menu", "gallery", "events"];
 
 module.exports = async (req, res) => {
   if (req.method !== "POST") return res.status(405).json({ error: "method_not_allowed" });
